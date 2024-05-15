e5edca8c1c1efdc81e82e0b8f19b5988cbfb3d65
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2024-05-15 20:12:15 +0300

    Add new locale layout and page components, remove favicon.ico and globals.css, and delete old layout and page components

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import "../../../styles/globals.css";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+
+export const metadata: Metadata = {
+    title: "Next.js Internationalization Example",
+    description: "An example of internationalization in a Next.js app",
+};
+
+type LocaleLayoutProps = Readonly<{
+    children: React.ReactNode;
+    params: {
+        locale: string;
+    };
+}>;
+
+export default async function LocaleLayout({
+    children,
+    params: { locale },
+}: LocaleLayoutProps) {
+    const messages = await getMessages();
+
+    return (
+        // Warning: Extra attributes from the server: cz-shortcut-listen
+
+        <html lang={locale}>
+            <body cz-shortcut-listen="true">
+                <NextIntlClientProvider messages={messages}>
+                    {children}
+                </NextIntlClientProvider>
+            </body>
+        </html>
+    );
+}

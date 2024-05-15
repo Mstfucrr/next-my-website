0f97394d97ed0887463ba34397e941456e7394f9
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2024-05-15 17:25:28 +0300

    Update CSS and add new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
+import CustomImage from "@/components/image";
+
+const press = [
+    "TheNewYorkTimes",
+    "TheWashingtonPost",
+    "Forbes",
+    "Bloomberg",
+    "BusinessInsider",
+    "TechCrunch",
+    "TheGuardian",
+    "Wired",
+];
+
 export default function Home() {
-  return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-red-400 underline">
-        Hello world!
-      </h1>
-    </div>
-  );
+    return (
+        <div className="w-full h-screen flex flex-col gap-y-5 justify-center items-center">
+            <h1 className="text-3xl font-bold text-red-400 underline">
+                Hello world!
+            </h1>
+            <div className="relative w-1/2">
+                <div className="group flex overflow-hidden p-2 [--gap:3rem] [gap:var(--gap)] flex-row max-w-full [--duration:5s]">
+                    {Array.from({ length: 5 }).map((_, i) => (
+                        <div
+                            className="flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row"
+                            key={i}
+                        >
+                            {press.map((name) => (
+                                <CustomImage
+                                    key={name}
+                                    src={`https://logo.clearbit.com/${name.toLowerCase()}.com`}
+                                    alt={name}
+                                    className="w-16 h-16"
+                                />
+                            ))}
+                        </div>
+                    ))}
+                    <div className="pointer-events-none absolute inset-y-0 left-0 h-full w-1/3 bg-gradient-to-r from-background"></div>
+                    <div className="pointer-events-none absolute inset-y-0 right-0 h-full w-1/3 bg-gradient-to-l from-background"></div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,41 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      size: {
-        screen: "100vh",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            size: {
+                screen: "100vh",
+            },
+        },
+        animation: {
+            marquee: "marquee var(--duration) infinite linear",
+        },
+        keyframes: {
+            marquee: {
+                "0%": { transform: "translateX(0)" },
+                "100%": { transform: "translateX(calc(-100% - var(--gap)))" },
+            },
+            "marquee-vertical": {
+                from: { transform: "translateY(0)" },
+                to: { transform: "translateY(calc(-100% - var(--gap)))" },
+            },
+        },
     },
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/container-queries"),
-  ],
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/container-queries"),
+    ],
 };
 export default config;
